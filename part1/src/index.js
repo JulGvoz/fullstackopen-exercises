9f262429e784ac2a832498617547f773f6042c9d
JulGvoz <julgvoz@tutanota.com>
2020-12-05 22:35:35 +0200

    Using components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello, {props.name}! You are {props.age} years old.</p>
+    </div>
+  )
+}
+
 const App = () => {
   const now = new Date()
   const a = 10
   const b = 20
+
+  const age = 15
+  const name = "James"
 
   return (
     <div>
@@ -12,6 +23,9 @@ const App = () => {
       <p>
         {a} + {b} = {a + b}
       </p>
+      <Hello name="George" age="10" />
+      <Hello name={name} age={10 + 25} />
+      <Hello name="Daisy" age={age} />
     </div>
   )
 }
