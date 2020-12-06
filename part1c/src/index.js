1dc66802429dfe92e942cff64272a79d5cd7347f
JulGvoz <julgvoz@tutanota.com>
2020-12-06 12:12:37 +0200

    Reuse functionality

--- part1c/src/index.js
+++ part1c/src/index.js
@@ -21,12 +21,14 @@ const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
       <Display counter={counter} />
       <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={decreaseByOne} text="minus" />
       <Button handleClick={setToZero} text="reset" />
     </div>
   )
