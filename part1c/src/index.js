6d6aff25540f25c9ddbfb40fb2015e7d31daf584
JulGvoz <julgvoz@tutanota.com>
2020-12-06 12:41:38 +0200

    Conditional rendering

--- part1c/src/index.js
+++ part1c/src/index.js
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-
-  const handleLeftClick = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
-
-  const handleRightClick = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      {clicks.left}
+      button press history: {allClicks.join(" ")}
+    </div>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
