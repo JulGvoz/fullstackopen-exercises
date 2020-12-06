3eaa15e432a5bb973d0bf97c93ad0c14ef475104
JulGvoz <julgvoz@tutanota.com>
2020-12-06 11:48:40 +0200

    Deconstructing the value

--- part1c/src/index.js
+++ part1c/src/index.js
@@ -2,18 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+  const { name, age } = props
 
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - age
-  }
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
+      <p>Hello {name}, you are {age} years old</p>
       <p>So you were probably born in {bornYear()}</p>
     </div>
   )
