81485aa4b00d1e4524c0e9d43734f8cc10565879
JulGvoz <julgvoz@tutanota.com>
2020-12-06 13:19:06 +0200

    Changing state with functions returning functions

--- part1c/src/index.js
+++ part1c/src/index.js
@@ -4,16 +4,14 @@ import ReactDOM from 'react-dom'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const hello = (who) => () => {
-    console.log('hello', who)
-  }
+  const setToValue = (newValue) => () => setValue(newValue)
 
   return (
     <div>
       {value}
-      <button onClick={hello("world")}>world</button>
-      <button onClick={hello("react")}>react</button>
-      <button onClick={hello("function")}>fun</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
