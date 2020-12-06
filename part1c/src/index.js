1bf3181ffdff29e7d1800499644ed5d05b6ab1e4
JulGvoz <julgvoz@tutanota.com>
2020-12-06 12:24:53 +0200

    Using object spread

--- part1c/src/index.js
+++ part1c/src/index.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <span>{counter}</span>
-
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      <Display counter={left} />
-      <Button handleClick={() => setLeft(left + 1)} text="left" />
-      <Button handleClick={() => setRight(right + 1)} text="right" />
-      <Display counter={right} />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
