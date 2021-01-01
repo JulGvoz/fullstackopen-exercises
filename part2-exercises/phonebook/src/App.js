239ad7440a088a1f62979b1bf613541fdb799f44
Jul Gvoz <julgvoz@tutanota.com>
2021-01-01 18:57:25 +0200

    Exercise 2.11

--- part2-exercises/phonebook/src/App.js
+++ part2-exercises/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filterState }) => {
   const [filterText, setFilterText] = filterState
@@ -56,15 +57,20 @@ const Persons = ({ persons, filterText }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
+
+  const hook = () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const handleNewPerson = (event) => {
     event.preventDefault()
